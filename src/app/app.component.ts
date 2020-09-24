8bd955595021f6b7203f73fd36f3b0b33fe89622
lioneltraore <lioneltraore@bitbucket.org>
2020-09-24 22:47:58 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tgl-calculator';
+  result = '';
+  typed = '';
+  passedFirst: boolean;
+
+  getNumber(number: string) {
+    this.typed += number;
+  }
+
+  getSymbol(symbol: string) {
+    // if (this.typed) {
+    //   if (symbol !== 'c' && symbol !== '%') {
+    //     switch (symbol) {
+    //       case '+':
+    //     }
+    //     this.typed += symbol;
+    //   }
+    // }
+  }
+
+  getResult() {}
 }
